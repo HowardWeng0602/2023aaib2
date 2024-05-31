f3f54a68acf64c5cc0aa982c9ec46146b3768874
HowardWeng0602 <145088372+HowardWeng0602@users.noreply.github.com>
2024-05-31 11:40:31 +0800

    Add files via upload

---
+++ week15-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        N = len(s)
+        for i in range(N):
+            print( s[i],t[i])# 先用 Python 把字串的迴圈寫出來
+
+        ans = 0
+        return ans
